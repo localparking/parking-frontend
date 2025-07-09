e3488f37efbfe2379fc4e84c12592bc10a2439cd
hin6150 <hin6150@gmail.com>
2025-07-09 22:40:57 +0900

    Refactor : 사용하지 않는 코드 제거

--- apps/react/src/app/chat/page.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/chat/')({
-  component: Chat,
-})
-
-function Chat() {
-  return <div className="m-auto text-2xl font-bold">채팅</div>
-}
--- apps/react/src/shared/services/auth.service.ts
+++ apps/react/src/shared/services/auth.service.ts
@@ -1,32 +1,32 @@
-import { AuthApi } from '@data/user-api-axios/api'
-import axios from 'axios'
-import apiInstance, { defaultOptions } from '../libs/api'
-import { LoginFormType } from '@/features/auth/model'
+// import { AuthApi } from '@data/user-api-axios/api'
+// import axios from 'axios'
+// import apiInstance, { defaultOptions } from '../libs/api'
+// import { LoginFormType } from '@/features/auth/model'
 
-class AuthService extends AuthApi {
-  constructor() {
-    super(undefined, '', apiInstance)
-  }
+// class AuthService extends AuthApi {
+//   constructor() {
+//     super(undefined, '', apiInstance)
+//   }
 
-  async getAuth() {
-    try {
-      const authApi = new AuthApi(undefined, '', axios.create(defaultOptions))
-      const { data } = await authApi.authControllerGetAuth()
-      return { data }
-    } catch (e: any) {
-      throw e
-    }
-  }
+//   async getAuth() {
+//     try {
+//       const authApi = new AuthApi(undefined, '', axios.create(defaultOptions))
+//       const { data } = await authApi.authControllerGetAuth()
+//       return { data }
+//     } catch (e: any) {
+//       throw e
+//     }
+//   }
 
-  async signIn({ accountId, password }: LoginFormType) {
-    const { data } = await this.authControllerPostAuth({
-      postAuthReqDto: {
-        accountId,
-        password,
-      },
-    })
-    return data
-  }
-}
+//   async signIn({ accountId, password }: LoginFormType) {
+//     const { data } = await this.authControllerPostAuth({
+//       postAuthReqDto: {
+//         accountId,
+//         password,
+//       },
+//     })
+//     return data
+//   }
+// }
 
-export default new AuthService()
+// export default new AuthService()
