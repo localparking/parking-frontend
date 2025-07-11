222eb3735bc7e0249a37456aa2a5d428174ba01d
hin6150 <hin6150@gmail.com>
2025-07-12 00:11:04 +0900

    Fix : 3000 포트 대신 3001 포트로 변경

--- apps/react/src/app/page.tsx
+++ apps/react/src/app/page.tsx
@@ -1,30 +1,10 @@
 import { useAuth } from '@/features/auth'
-import bridge from '@/shared/bridge'
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
-import { Button } from '@ui/common/components/button'
 
 export const Route = createFileRoute('/')({
   component: HomePage,
 })
 
 function HomePage() {
-  const auth = useAuth()
-  const navigate = useNavigate()
-
-  return (
-    <div>
-      <Button
-        onClick={async () => {
-          const data = await auth.logout()
-          if (data.success) {
-            console.log('로그아웃 성공:', data.message)
-            navigate({ to: '/login', replace: true })
-          }
-        }}
-      >
-        임시 로그아웃
-      </Button>
-      HomePage
-    </div>
-  )
+  return <div>HomePage</div>
 }
