d7d9d90bdc65ebc00fecb7b90e4915fb05e7bf02
jinu <workspace_@naver.com>
2025-07-17 22:14:57 +0900

    chore: .gitignore 업데이트 및  Vite 설정 수정

--- apps/react/vite.config.ts
+++ apps/react/vite.config.ts
@@ -2,85 +2,65 @@ import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import svgr from 'vite-plugin-svgr'
 
-export default defineConfig({
-  build: {
-    terserOptions: {
-      compress: {
-        drop_console: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    build: {
+      terserOptions: {
+        compress: {
+          drop_console: true,
+        },
       },
     },
-  },
-  plugins: [
-    TanStackRouterVite({
-      target: 'react',
-      autoCodeSplitting: true,
-      routesDirectory: 'src/app',
-      indexToken: 'page',
-      routeToken: 'layout',
-    }),
-    react(),
-    tailwindcss(),
-    svgr({
-      svgrOptions: {
-        icon: true,
-        titleProp: true,
-      },
-      include: '**/*.svg',
-    }),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: '@ui/common',
-        replacement: path.resolve(__dirname, '../../packages/ui/common/src'),
-      },
-      {
-        find: '@mobile/bridge',
-        replacement: path.resolve(__dirname, '../mobile/app/bridge'),
-      },
-      {
-        find: '@/',
-        replacement: `${path.resolve(__dirname, 'src')}/`,
-      },
-      {
-        find: '@/types',
-        replacement: `${path.resolve(__dirname, 'src/shared/types')}/`,
-      },
-      {
-        find: '@/constants',
-        replacement: `${path.resolve(__dirname, 'src/shared/constants')}/`,
-      },
-      {
-        find: '@/utils',
-        replacement: `${path.resolve(__dirname, 'src/shared/utils')}/`,
-      },
-      {
-        find: '@/hooks',
-        replacement: `${path.resolve(__dirname, 'src/shared/hooks')}/`,
-      },
-      {
-        find: '@/libs',
-        replacement: `${path.resolve(__dirname, 'src/shared/libs')}/`,
-      },
+    plugins: [
+      TanStackRouterVite({
+        target: 'react',
+        autoCodeSplitting: true,
+        routesDirectory: 'src/app',
+        indexToken: 'page',
+        routeToken: 'layout',
+      }),
+      react(),
+      tailwindcss(),
+      svgr({
+        svgrOptions: {
+          icon: true,
+          titleProp: true,
+        },
+        include: '**/*.svg',
+      }),
     ],
-  },
-  server: {
-    port: 3001,
-    proxy: {
-      '/api': {
-        target: 'https://townparking.store',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        cookieDomainRewrite: {
-          '*': 'localhost',
-        },
-        cookiePathRewrite: {
-          '*': '/',
+    resolve: {
+      alias: [
+        { find: '@ui/common', replacement: path.resolve(__dirname, '../../packages/ui/common/src') },
+        { find: '@mobile/bridge', replacement: path.resolve(__dirname, '../mobile/app/bridge') },
+        { find: '@/', replacement: `${path.resolve(__dirname, 'src')}/` },
+        { find: '@/types', replacement: `${path.resolve(__dirname, 'src/shared/types')}/` },
+        { find: '@/constants', replacement: `${path.resolve(__dirname, 'src/shared/constants')}/` },
+        { find: '@/utils', replacement: `${path.resolve(__dirname, 'src/shared/utils')}/` },
+        { find: '@/hooks', replacement: `${path.resolve(__dirname, 'src/shared/hooks')}/` },
+        { find: '@/libs', replacement: `${path.resolve(__dirname, 'src/shared/libs')}/` },
+      ],
+    },
+    server: {
+      port: 3001,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          cookieDomainRewrite: {
+            '*': 'localhost',
+          },
+          cookiePathRewrite: {
+            '*': '/',
+          },
         },
       },
     },
-  },
+  }
 })
