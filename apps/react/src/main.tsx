0479ee2a391bb579c537a31c0c99a2c1fac1eb78
hin6150 <hin6150@gmail.com>
2025-11-03 14:35:00 +0900

    Feat: 모바일 스크롤 바운스 제거

--- apps/react/src/main.tsx
+++ apps/react/src/main.tsx
@@ -2,6 +2,11 @@ import ReactDOM from 'react-dom/client'
 import { App } from './app'
 import './main.css'
 
+document.body.classList.add('no-bounce-scroll')
+document.documentElement.classList.add('no-bounce-scroll')
+document.documentElement.style.overscrollBehavior = 'none'
+
+
 const rootElement = document.getElementById('app')!
 
 if (!rootElement.innerHTML) {
